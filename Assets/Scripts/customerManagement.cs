0464c57c651acd5386d33fcca600d67b6f41f108
ualogic <uatihb@gmail.com>
2017-03-18 16:48:34 +0000

    mock5

---
+++ Assets/Scripts/customerManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Cusomer {
+
+	public GameObject obj;
+	public int mostSuitableAge;
+	public int difficulty;
+	public int probability;
+
+}
+
+public class customerManagement : MonoBehaviour {
+
+	public Cusomer[] cusomers;
+
+}
--- Assets/Scripts/productManagement.cs
+++ Assets/Scripts/productManagement.cs
@@ -2,6 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Product {
+
+	public GameObject obj;//{ get; set; }
+//	public bool ageRestricted;//{ get; set; } //Ideas for the future
+	public int mostSuitableAge;//{ get; set; }
+	public int difficulty;//{ get; set; }
+	public int probability;//{ get; set; }
+
+//	public Product(GameObject Obj, int[] AgeRange, int MostSuitableAge, int Difficulty, int Probability){
+//
+//		obj = Obj;
+//		ageRange = AgeRange;
+//		mostSuitableAge = MostSuitableAge;
+//		difficulty = Difficulty;
+//		probability = Probability;
+//	
+//	}
+
+
+
+
+
+}
+
 
 /*Instantiates new products and translates them closer to a til point*/
 public class productManagement : MonoBehaviour {
@@ -9,11 +34,15 @@ public class productManagement : MonoBehaviour {
 //	public float speed = 100.0f;
 //	int counter = 0;
 
-	public GameObject product;
-	public Transform newProdPos;
+
+//	public GameObject[] obj;
+	public Transform spawningPoint;
+
+	public Product[] products;
 	// Use this for initialization
 	void Start () {
 //		StartCoroutine ("Instance");
+
 	}
 	
 	// Update is called once per frame
